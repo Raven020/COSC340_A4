bb3319fe6cf65e41916e73978a403e675aeb6db1
Raven Clodd <raven.c1@outlook.com>
2023-04-01 19:18:15 +1000

    Functionality almost fuilly complete, need to work on handling erors

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 
-""" 
+''' 
 Connect - Upon starting client will retrieve userID from user then
 send 'CONNECT userID' to server to connect
 Put - PUT {key} then client will prompt user for value to put before sending
@@ -10,10 +10,11 @@ Disconnect - DISCONNECT then client will send to server to disconnect from serve
 
 
 
-"""
+'''
 HOST = '127.0.0.1'
 PORT = 1234
-Protocol = ['PUT', 'GET', 'DELETE', 'DISCONNECT']
+PROTOCOL = ['PUT', 'GET', 'DELETE', 'DISCONNECT']
+RESPONSES = ['OK', 'ERROR']
 
 
 def Connect(sock):
@@ -23,13 +24,14 @@ def Connect(sock):
 
 
 def Put(key, sock):
-    value = input(f"Please enter value you would like to store under {key}: ")
-    sock.sendall(bytes('PUT ' + ''.join(key) + " " + value, 'utf8'))
+    value = input(f'Please enter value you would like to store under {key}: ')
+    toStore = key + "," + value
+    sock.sendall(bytes('PUT ' + toStore, 'utf8'))
     return None
 
 
 def Get(key, sock):
-    sock.sendall(bytes('GET ' + ''.join(key), 'utf8'))
+    sock.sendall(bytes('GET ' + ''.join(key[0]), 'utf8'))
     return None
 
 
@@ -43,6 +45,19 @@ def Disconnect(sock):
     return None
 
 
+def ProcessData(data):
+    dataItems = data.split(" : ")
+    action = dataItems[0]
+    status = dataItems[1]
+    # TODO get doesn't return OK just the value. What should the process be for GET actions
+    if action in PROTOCOL or action != "GET" and status not in RESPONSES:
+        if action == "DISCONNECT":
+            print(data)
+            exit()
+        return True
+    return False
+
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((HOST, PORT))
@@ -50,36 +65,42 @@ def main():
         # get user id and send Connect request to server
 
         userId = Connect(sock)
+        data = sock.recv(2048).decode('utf-8')
+        if(data == 'CONNECT : OK'):
+            while True:
+                # wait for input
+                userInput = input('Enter input: ')
+                if userInput == '' or ' ' not in userInput:
+                    print('Not a valid Input Please try again')
+                    continue
+                userWords = userInput.split()
+                userWords[0] = userWords[0].upper()
+                if(userWords[0] in PROTOCOL):
+                    # if input is valid then send data
+                    # call apropriate function
+                    if userWords[0] == 'PUT':
+                        Put(''.join(userWords[1:]), sock)
+                    elif userWords[0] == 'GET':
+                        Get(userWords[1:], sock)
+                    elif userWords[0] == 'DELETE':
+                        Delete(userWords[1:], sock)
+                    elif userWords[0] == 'DISCONNECT':
+                        Disconnect(sock)
+                else:
+                    print('Not a valid Input Please try again')
+                    continue
+                data = sock.recv(2048).decode('utf-8')
+                if(data != None):
+                    if ProcessData(data):
+                        print(data)
 
-        while True:
-            # wait for input
-            userInput = input('Enter input: ')
-            userWords = userInput.split()
-            if(userWords[0] in Protocol and len(userWords) < 4):
-                # if input is valid then send data
-                # call apropriate function
-                if userWords[0] == 'PUT':
-                    Put(userWords[1:], sock)
-                elif userWords[0] == 'GET':
-                    Get(userWords[1:], sock)
-                elif userWords[0] == 'DELETE':
-                    Delete(userWords[1:], sock)
-                elif userWords[0] == 'DISCONNECT':
-                    Disconnect(sock)
-            else:
-                print("Not a valid Input Please try again")
-                continue
-            data = sock.recv(2048)
-            if(data == b'message recieved'):
-                print(data)
-                data = None
-            if(data == b'DISCONNECT: OK'):
-                # The case in which the server is disconnecting
-                print(data)
-                exit()
+                    else:
+                        print(
+                            "There was an error with the server, Disconnecting" + data)
+                        exit()
 
     # recieve output from server make appropriate action
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 
-"""
+'''
 CONNECT {userId} - Server will create or open file with that userID into memory, return OK to client
 PUT {key} value - Server will store the value under the key in memory, to be saved later, return OK to client
 GET {key} - Retrieve value from memory, that corresponds to that key, return OK to client
@@ -9,28 +9,56 @@ DELETE {key} - Remove the value at given key, return OK to client
 DISCONNECT - Disconnect from the client, return OK to client
 
 
-"""
+'''
 HOST = ''
 PORT = 1234
+User = str()  # username
+Storage = dict()  # the that is in data in memory
 
 
-def Connect(userID):
+def Connect(userID, sock):
+    # TODO read in users file, read map into memory
+    User = userID
+    print(User + ' has connected')
+    sock.sendall(b'CONNECT : OK')
     return None
 
 
-def Put(key, value):
+def Put(data, sock):
+    dataItems = data[0].split(",")
+    key = dataItems[0]
+    value = dataItems[1]
+    Storage[key] = value
+    sock.sendall(b'PUT : OK')
+    print(Storage)
     return None
 
 
-def Get(key, value):
+def Get(data, sock):
+
+    if(data[0] in Storage.keys()):
+        print(bytes(Storage[data[0]], 'utf-8'))
+        sock.sendall(bytes("GET : " + Storage[data[0]], 'utf-8'))
+    else:
+        sock.sendall(bytes("GET : ERROR", 'utf-8'))
     return None
 
 
-def Delete(key, value):
+def Delete(data, sock):
+    # print(data[0])
+    try:
+        Storage.pop(data[0])
+        sock.sendall(bytes("DELETE : OK", 'utf-8'))
+    except:
+        sock.sendall(bytes("DELETE : ERROR"), 'utf-8')
+    print(Storage)
+
     return None
 
 
-def Disconnect():
+def Disconnect(userID, sock):
+    print('' + userID + ' has disconnected')
+    sock.sendall(b'DISCONNECT : OK')
     return None
 
 
@@ -54,21 +82,31 @@ def main():
     conn, address = clientSoc.accept()
 
     while True:
-        print('Waiting for data')
+        # print('Waiting for data')
         data = conn.recv(2048)
-        if data != None:
-            data = str(data)[1:]
-            print(data)
-            if data == "'DISCONNECT'" or data == '':
-                conn.sendall(b'DISCONNECT: OK')
+        if data:
+            data = data.decode('utf-8')
+            dataWords = data.split(' ', 1)
+            if dataWords[0] == 'CONNECT':
+                Connect(''.join(dataWords[1:]), conn)
+            elif dataWords[0] == 'PUT':
+                Put(dataWords[1:], conn)
+            elif dataWords[0] == 'GET':
+                Get(dataWords[1:], conn)
+            elif dataWords[0] == 'DELETE':
+                Delete(dataWords[1:], conn)
+            elif dataWords[0] == 'DISCONNECT':
+                Disconnect(User, conn)
                 break
-            #conn.sendall(b"message recieved")
-            data = None
+            else:
+                print('Not a valid input disconnecting from client')
+                conn.close()
+            # conn.sendall(b'message recieved')
 
-    print('Connected with' + address[0] + ':' + str(address[1]))
+    # print('Connected with ' + address[0] + ':' + str(address[1]))
 
     clientSoc.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
